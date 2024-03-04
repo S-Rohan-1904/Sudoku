3bb7ff9b476716d04226cbe43564b4d2e86c75ce
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2024-03-04 17:50:18 +0530

    Update win checker and validation

--- src/components/GameCanvas.js
+++ src/components/GameCanvas.js
@@ -4,21 +4,30 @@ import generateSudoku from "./SudokuGenerator";
 import validator from "./InputValidator";
 import { useState, useEffect } from "react";
 
-const winChecker = (sudoku, data, rindex, cindex) => {
-  let win;
+const winChecker = (sudoku, data, rindex, cindex, isValid) => {
+  let validity = true;
   for (let i = 0; i < 9; i++) {
-    win = false;
-    if (!sudoku[i].includes("") && validator(rindex, cindex, data, sudoku)) {
-      win = true;
-    } else {
+    if (isValid[i].includes(false)) {
+      validity = false;
       break;
     }
   }
-  if (win) {
-    alert("You won!!"); // handle what to do after win
+  let win;
+  if (validity) {
+    for (let i = 0; i < 9; i++) {
+      win = false;
+      if (!sudoku[i].includes("")) {
+        win = true;
+      } else {
+        break;
+      }
+    }
+    if (win) {
+      alert("You won!!"); // handle what to do after win
+    }
   }
 };
-const initialSudoku = generateSudoku(45);
+const initialSudoku = generateSudoku(3);
 function GameCanvas() {
   const validationArr = Array(9);
   for (let i = 0; i < 9; i++) {
@@ -29,7 +38,7 @@ function GameCanvas() {
 
   const inputChangeHandler = (data, rindex, cindex) => {
     if (data == 0) {
-      // beacuse it is converting to 0 because of +
+      // because it is converting to 0 because of +
       data = "";
     }
 
@@ -43,15 +52,17 @@ function GameCanvas() {
       });
     });
 
-    let validationArrCopy = [...isValid];
-    if (!validator(rindex, cindex, data, sudokuArr)) {
-      validationArrCopy[rindex][cindex] = false;
-      setIsValid(validationArrCopy);
-    } else {
-      validationArrCopy[rindex][cindex] = true;
-      setIsValid(validationArrCopy);
-    }
-    winChecker(sudokuArr, data, rindex, cindex);
+    const updatedValidationArr = validationArr.map((row, rowIndex) => {
+      return row.map((cell, colIndex) => {
+        if (rowIndex == rindex && colIndex == cindex) {
+          return validator(rindex, cindex, data, sudokuArr);
+        } else {
+          return isValid[rowIndex][colIndex];
+        }
+      });
+    });
+    setIsValid(updatedValidationArr);
+    winChecker(sudokuArr, data, rindex, cindex, updatedValidationArr);
     setSudokuArr(updatedSudokuArr);
   };
   return (
